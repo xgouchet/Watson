c49d36fde7ff6919dba8a975050c7cb192b9c386
xaviergouchet <xg@deezer.com>
2013-09-27 09:48:59 +0200

    App Structure

--- Watson/src/fr/xgouchet/webmonitor/activity/TargetActivity.java
+++ Watson/src/fr/xgouchet/webmonitor/activity/TargetActivity.java
@@ -1,8 +1,37 @@
 package fr.xgouchet.webmonitor.activity;
 
-import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import fr.xgouchet.webmonitor.ui.TargetFragment;
 
 
-public class TargetActivity extends Activity {
+public class TargetActivity extends FragmentActivity {
     
+    //////////////////////////////////////////////////////////////////////////////////////
+    // Activity Lifecycle
+    //////////////////////////////////////////////////////////////////////////////////////
+    
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // start dialog fragment
+        FragmentManager mgr = getSupportFragmentManager();
+        FragmentTransaction transaction = mgr.beginTransaction();
+        TargetFragment target = new TargetFragment();
+        target.show(transaction, "");
+        
+        // finish activity when the fragment closes
+        target.setOnDismissListener(new OnDismissListener() {
+            
+            @Override
+            public void onDismiss(final DialogInterface dialog) {
+                finish();
+            }
+        });
+    }
 }
--- Watson/src/fr/xgouchet/webmonitor/data/Constants.java
+++ Watson/src/fr/xgouchet/webmonitor/data/Constants.java
@@ -3,6 +3,10 @@ package fr.xgouchet.webmonitor.data;
 
 public final class Constants {
     
+    //////////////////////////////////////////////////////////////////////////////////////
+    // Time
+    //////////////////////////////////////////////////////////////////////////////////////
+    
     public static final long SECOND = 1000;
     public static final long MINUTE = SECOND * 60;
     public static final long HOUR = MINUTE * 60;
@@ -10,6 +14,41 @@ public final class Constants {
     public static final long WEEK = DAY * 7;
     public static final long MONTH = DAY * 28;
     
+    public static final long[] FREQUENCIES = new long[] {
+            DAY / 2, DAY,
+            WEEK / 2, WEEK, MONTH / 2, MONTH
+    };
+    
+    public static final int[] DIFFERENCES = new int[] {
+            0, 10, 25, 50, 100,
+            250, 500
+    };
+    
+    //////////////////////////////////////////////////////////////////////////////////////
+    // Preferences
+    //////////////////////////////////////////////////////////////////////////////////////
+    
+    public static final String PREFERENCES_NAME = "fr.xgouchet.websiteupdate";
+    
+    public static final String PREF_BLINK_LED = "blink_led";
+    public static final String PREF_LED_COLOR = "led_color";
+    public static final String PREF_BLINK_LED_ERROR = "blink_led_error";
+    public static final String PREF_LED_ERROR_COLOR = "led_error_color";
+    public static final String PREF_WIFI_ONLY = "wifi_only";
+    public static final String PREF_ALLOW_ROAMING = "allow_roaming";
+    public static final String PREF_DEFAULT_FREQUENCY = "default_frequency";
+    public static final String PREF_DEFAULT_DIFFERENCE = "default_difference";
+    
+    //////////////////////////////////////////////////////////////////////////////////////
+    // Actions
+    //////////////////////////////////////////////////////////////////////////////////////
+    
+    public static final String EXTRA_ACTION = "action";
+    public static final String EXTRA_TARGET = "target";
+    
+    public static final String ACTION_EDIT_TARGET = "fr.xgouchet.webmonitor.ACTION_EDIT_TARGET";
+    public static final String ACTION_NEW_TARGET = "fr.xgouchet.webmonitor.ACTION_NEW_TARGET";
+    public static final String ACTION_CHECK_TARGET = "fr.xgouchet.webmonitor.ACTION_CHECK_TARGET";
     
     private Constants() {
     }
